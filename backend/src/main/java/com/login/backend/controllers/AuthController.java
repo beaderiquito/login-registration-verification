2a2cc45a282772d5c7d3ac7ce96dbf6d089115b7
Bea Deriquito <beaderiquito@gmail.com>
2021-12-13 21:19:30 +0800

    Login now working

---
+++ backend/src/main/java/com/login/backend/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.login.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class AuthController {
+    @GetMapping("/login")
+    public void login() {
+
+    }
+}
